2a297db45d56d5be3d85fe841b6e1b8fecbd79c7
Kshirabdhi Routray <little.1462@gmail.com>
2015-03-09 23:10:49 -0500

    Added a new custom cell

---
+++ SampleProject1/CustomCell/CustomCell.h
@@ -0,0 +1,13 @@
+//
+//  CustomCell.h
+//  SampleProject1
+//
+//  Created by Kshirabdhi Routray on 3/9/15.
+//  Copyright (c) 2015 Kshirabdhi Routray. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CustomCell : UITableViewCell
+
+@end
---
+++ SampleProject1/CustomCell/CustomCell.m
@@ -0,0 +1,23 @@
+//
+//  CustomCell.m
+//  SampleProject1
+//
+//  Created by Kshirabdhi Routray on 3/9/15.
+//  Copyright (c) 2015 Kshirabdhi Routray. All rights reserved.
+//
+
+#import "CustomCell.h"
+
+@implementation CustomCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
